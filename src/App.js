c7bbebf74d4d55a212ce090f572590578700e285
RodroVMS <democlesvanshepars@gmail.com>
2021-11-11 14:40:07 -0500

    front end of game base project of _buildspace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+import SelectCharacter from './Components/SelectCharacter';
+import {CONTRACT_ADDRESS, transformCharacterData} from './constants';
+import game from './utils/Game.json';
+import { ethers } from 'ethers';
+import Arena from './Components/Arena';
+import LoadingIndicator from "./Components/LoadingIndicator"
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -8,17 +14,20 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState(null)
+  const [characterNFT, setCharacterNFT] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   
-  const checkIfWalletIsConnected = () => {
+  const checkIfWalletIsConnected = async () => {
     try {
       const {ethereum} = window;
       if (!ethereum) {
         console.log("MetaMask not detected! Get MetaMask");
+        setIsLoading(false);
         return;
       }
       console.log("Wallet Connected:", ethereum);
       const accounts = await ethereum.request({method: 'eth_accounts'});
-        if (accounts.length != 0) {
+        if (accounts.length !== 0) {
           const account = accounts[0];
           console.log('Found an authorized account:', account);
           setCurrentAccount(account)
@@ -28,23 +37,95 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
+    setIsLoading(false);
   }
+  
+  const connectWalletAction = async() => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert('Get Metamask');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log("Connected", accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
     
   useEffect(() => {
+    setIsLoading(true);
     checkIfWalletIsConnected();
   }, []);
-   
+
+  useEffect(() => {
+    const fetchNFTMetadata = async () => {
+      console.log("Checking for Character NFT on address:", currentAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        game.abi,
+        signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      console.log("Check if user has NFT Completed", txn);
+      if (txn.name) {
+        console.log('User has character NFT');
+        setCharacterNFT(transformCharacterData(txn));
+      }
+      setIsLoading(false);
+    };
+    if (currentAccount) {
+      console.log('Current account:', currentAccount);
+      fetchNFTMetadata();
+    }
+
+  }, [currentAccount]);
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+            alt="Monty Python Gif"
+          />
+          <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+          >
+              Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+    } 
+    else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT}/>;
+    } else if (currentAccount && characterNFT) {
+      return <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT}/>
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚔️ Metaverse Pirate⚔️</p>
+          <p className="sub-tex">Team up to find the NFT One Piece</p>
           <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
+            {renderContent()}
           </div>
         </div>
         <div className="footer-container">
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x350CaeF828c7b2Ee9EC92d3b1D7684F968A2b23E';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData }
